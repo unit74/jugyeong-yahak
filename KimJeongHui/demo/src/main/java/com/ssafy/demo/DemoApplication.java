b69899d9b053fb20f7353b393dc09df92095eb89
김정희 <spor1998@naver.com>
2023-07-18 18:00:57 +0900

    [feat] 기본 세팅

---
+++ KimJeongHui/demo/src/main/java/com/ssafy/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.ssafy.demo;
+
+import com.ssafy.user.model.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		User user = new User("kim","010");
+		System.out.println(user);
+	}
+
+}
---
+++ KimJeongHui/demo/src/main/java/com/ssafy/user/model/User.java
@@ -0,0 +1,15 @@
+package com.ssafy.user.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String name;
+    private String phone;
+
+}
