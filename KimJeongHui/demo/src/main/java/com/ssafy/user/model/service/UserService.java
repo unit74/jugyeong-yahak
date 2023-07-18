93558ccf59e774aee2808ac4e5278dea2eeceef8
김정희 <spor1998@naver.com>
2023-07-18 18:00:57 +0900

    [feat] 기본 세팅

---
+++ KimJeongHui/demo/src/main/java/com/ssafy/user/model/service/UserService.java
@@ -0,0 +1,8 @@
+package com.ssafy.user.model.service;
+
+import com.ssafy.user.model.User;
+
+public interface UserService {
+
+    void login(User user);
+}
---
+++ KimJeongHui/demo/src/main/java/com/ssafy/user/model/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ssafy.user.model.service;
+
+import com.ssafy.user.model.User;
+
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public void login(User user) {
+
+        return;
+    }
+}
