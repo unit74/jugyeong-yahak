3bc9c8482992e9f23934b11132cef1af9a5e5816
spor1998 <spor1998@naver.com>
2023-07-21 00:14:14 +0900

    [feat] openVidu(Spring Server, React) 기초 세팅

---
+++ KimJeongHui/yahack/src/main/java/com/ssafy/yahack/lesson/LessonController.java
@@ -0,0 +1,7 @@
+package com.ssafy.yahack.lesson;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LessonController {
+}
---
+++ KimJeongHui/yahack/src/test/java/com/ssafy/yahack/YahackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.yahack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
