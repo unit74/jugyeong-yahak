d57d6a064aafc4155327ebec001c9ccd84155f9f
spor1998 <spor1998@naver.com>
2023-07-29 15:11:42 +0900

    jeonghui

--- KimJeongHui/yahack/src/test/java/com/ssafy/yahack/YahackApplicationTests.java
+++ KimJeongHui/yahack/src/test/java/com/ssafy/yahack/YahackApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.yahack;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class YahackApplicationTests {
 
 	@Test
