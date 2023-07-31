914f0cf7c9ddc8e8fae835eb398c4d3b888b6ff3
Yejin Kim <yeajin7933@gmail.com>
2023-07-31 11:28:36 +0900

    Update Fe router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,14 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 // pages
 import StudentMain from "./pages/Student/StudentMain";
 import Error404 from "./pages/Common/Error404";
+import TeacherMain from "./pages/Teacher/TeacherMain";
 
 function App() {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<StudentMain />} />
-
+        <Route path="/teachermain" element={<TeacherMain />} />
         {/* 나머지 라우터 여기에 설계할 것 */}
         <Route path="*" element={<Error404 />} />
       </Routes>
