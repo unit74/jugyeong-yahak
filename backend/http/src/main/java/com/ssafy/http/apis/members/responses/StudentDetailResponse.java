cfb19f72f940a817178c17abc06ba2f6a72a633d
unit74 <u__nit@naver.com>
2023-08-01 21:55:26 +0900

    feat: 학생 한 명 상세조회 완료, 한 반에 대한 학생 조회 작업중

---
+++ backend/http/src/main/java/com/ssafy/http/apis/members/responses/StudentDetailResponse.java
@@ -0,0 +1,46 @@
+package com.ssafy.http.apis.members.responses;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.ssafy.http.apis.members.entities.MemberEntity;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StudentDetailResponse {
+
+    private Long id;
+    private Long governmentId;
+    private Long classId;
+    private String role;
+    private String statusCode;
+    private String name;
+    private String phone;
+    private String address;
+    private String faceImageUrl;
+    private String firstResponder;
+    private String tabletNo;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public void of(MemberEntity memberEntity) {
+        this.id = memberEntity.getId();
+        this.governmentId = memberEntity.getGovernmentId();
+        this.classId = memberEntity.getClassId();
+        this.role = memberEntity.getRole()
+                                .getRole();
+        this.statusCode = memberEntity.getStatusCode();
+        this.name = memberEntity.getName();
+        this.phone = memberEntity.getPhone();
+        this.address = memberEntity.getAddress();
+        this.faceImageUrl = memberEntity.getFaceImageUrl();
+        this.firstResponder = memberEntity.getFirstResponder();
+        this.tabletNo = memberEntity.getTabletNo();
+        this.createdAt = memberEntity.getCreatedAt();
+        this.updatedAt = memberEntity.getUpdatedAt();
+    }
+}
