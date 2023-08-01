aeaeb8eaf4f692cabccadbcbbef43bce4debc9fd
seongho <tjdghgk98@gmail.com>
2023-08-02 01:37:54 +0900

    feat : S3에 이미지 업로드 코드 작성 완료. -> ~/private/busan/register 후, body에 multipart로 image라는 이름으로 이미지파일 전송

--- backend/http/src/main/java/com/ssafy/http/apis/members/controllers/MemberPrivateController.java
+++ backend/http/src/main/java/com/ssafy/http/apis/members/controllers/MemberPrivateController.java
@@ -1,5 +1,25 @@
 package com.ssafy.http.apis.members.controllers;
 
-public class MemberPrivateController {
+import com.ssafy.http.apis.members.services.S3ImageUploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+public class MemberPrivateController {
+    private final S3ImageUploadService imageUploadService;
+    @PostMapping(value="/private/{government}/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String memberLogin(@PathVariable String government, @RequestParam(value="image") MultipartFile image) throws IOException {
+
+        System.out.println("이미지 등록 요청 받음");
+        System.out.println(government);
+
+        String fileName = imageUploadService.uploadImage(government, image);
+        return fileName;
+    }
 }
