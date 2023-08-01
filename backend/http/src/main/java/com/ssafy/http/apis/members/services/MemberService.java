2e4eacc5c1976eeb5f4b3e163258e34549675ef7
unit74 <u__nit@naver.com>
2023-08-01 23:05:20 +0900

    feat: 반 학생 전체 조회

--- backend/http/src/main/java/com/ssafy/http/apis/members/services/MemberService.java
+++ backend/http/src/main/java/com/ssafy/http/apis/members/services/MemberService.java
@@ -1,9 +1,12 @@
 package com.ssafy.http.apis.members.services;
 
+import com.ssafy.http.apis.members.entities.MemberEntity;
 import com.ssafy.http.apis.members.repositories.MemberRepository;
 import com.ssafy.http.apis.members.responses.StudentDetailResponse;
 import com.ssafy.http.exception.WrongParameterException;
 import com.ssafy.http.support.codes.ErrorCode;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +16,30 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public StudentDetailResponse getStudentDetail(Long id) {
+    public StudentDetailResponse getStudentDetail(Long studentId) {
         StudentDetailResponse studentDetailResponse = new StudentDetailResponse();
 
-        studentDetailResponse.of(memberRepository.findById(id)
+        studentDetailResponse.of(memberRepository.findById(studentId)
                                                  .orElseThrow(
                                                      () -> new WrongParameterException(
                                                          ErrorCode.BAD_REQUEST_ERROR)));
 
         return studentDetailResponse;
+    }
+
+    public List<StudentDetailResponse> getClassStudents(Long classId) {
+        List<StudentDetailResponse> studentDetailResponses = new ArrayList<>();
+
+        List<MemberEntity> students = memberRepository.findAllByClassId(classId);
+        
+        for (MemberEntity student : students) {
+            StudentDetailResponse studentDetailResponse = new StudentDetailResponse();
+
+            studentDetailResponse.of(student);
+
+            studentDetailResponses.add(studentDetailResponse);
+        }
+
+        return studentDetailResponses;
     }
 }
