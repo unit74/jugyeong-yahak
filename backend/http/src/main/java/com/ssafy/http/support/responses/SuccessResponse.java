f466a340065b4983a59a74db7ab436ab1776d477
unit74 <u__nit@naver.com>
2023-08-01 16:44:30 +0900

    feat: 되는 기능들에 대해 아래에 명시함
    Exception handling, API response integrated, Error and Success codes handling, Government Auth API(Login, Logout, etc..), JWT Refresh Token Redis handling, Security config, Swagger config
    
    해야할 것 : Security Utils(Context holder에서 User의 PK를 가져오는 역할), 학생 CRUD API 작성

---
+++ backend/http/src/main/java/com/ssafy/http/support/responses/SuccessResponse.java
@@ -0,0 +1,38 @@
+package com.ssafy.http.support.responses;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.ssafy.http.support.codes.SuccessCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SuccessResponse<T> {
+
+    private Integer status;
+    private String message;
+    private T data;
+
+    public static <T> SuccessResponse<T> ofStatus(final SuccessCode code) {
+        return new SuccessResponse<>(code.getStatus(), code.getMessage(), null);
+    }
+
+    public static <T> SuccessResponse<T> ofStatusAndMessage(final SuccessCode code,
+        String message) {
+        return new SuccessResponse<>(code.getStatus(), message, null);
+    }
+
+    public static <T> SuccessResponse<T> ofStatusAndData(final SuccessCode code, T data) {
+        return new SuccessResponse<>(code.getStatus(), null, data);
+    }
+
+    public static <T> SuccessResponse<T> ofStatusAndMessageAndData(final SuccessCode code,
+        String message,
+        T data) {
+        return new SuccessResponse<>(code.getStatus(), message, data);
+    }
+}
