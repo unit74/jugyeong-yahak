1041f5d6cc6a67f11c0f37ffa62092f646e318ca
seongho <tjdghgk98@gmail.com>
2023-08-02 07:49:20 +0900

    feat : 노드에 얼굴인식 로그인을 요청보내고, 받는 service. 현재 resttemplate 무한 루프로 기다리는데, 이거  해결해야함

--- backend/http/src/main/java/com/ssafy/http/apis/members/services/MemberService.java
+++ backend/http/src/main/java/com/ssafy/http/apis/members/services/MemberService.java
@@ -1,33 +1,41 @@
 package com.ssafy.http.apis.members.services;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Optional;
 
 
 @Service
 public class MemberService {
 
-    public String memberLogin(MultipartFile faceImage, String government) throws URISyntaxException {
+    public String memberLogin(MultipartFile faceImage, String government) throws URISyntaxException, IOException {
+
+        File uploadFile = convert(faceImage)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
+
 
         LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
 
-        //String[] labels = new String[1];
-        //labels[0] = "lee";
+        String[] labels = new String[1];
+        labels[0] = "lee";
 
-        //params.add("labels", labels);
-        params.add("face-image", faceImage);
+        params.add("labels", labels);
+        //params.add("Face", faceImage);
+        params.add("Face", uploadFile);
+
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
@@ -39,14 +47,46 @@ public class MemberService {
 
         RestTemplate rest = new RestTemplate();
 
+        //이거 추가?
+        //rest.getMessageConverters().add(new ByteArrayHttpMessageConverter());
+
         ResponseEntity<JsonNode> postForEntity
                 = rest.postForEntity(nodeUrl, request, JsonNode.class);
 
-        System.out.println(postForEntity.getStatusCodeValue());
-        System.out.println(postForEntity.getBody());
+        removeNewFile(uploadFile);
+
+        //System.out.println(postForEntity.getStatusCodeValue());
+
+        System.out.println(postForEntity.getBody().get("result").get(0).get("_label").asText());
+        System.out.println(postForEntity.getBody().get("result").get(0).get("_distance").asDouble());
+        //{"result":[{"_label":"lee","_distance":0.3462712649992261}]}
         System.out.println(postForEntity.getHeaders());
 
         return "hello";
 
+    }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+
+        File convertFile = new File(file.getOriginalFilename());
+
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+    }
+
+    private void removeNewFile(File targetFile) {
+        if(targetFile.delete()) {
+            //log.info("파일이 삭제되었습니다.");
+            System.out.println("파일 삭제되었습니다.");
+        }else {
+            //log.info("파일이 삭제되지 못했습니다.");
+            System.out.println("파일 삭제되지 않음");
+        }
     }
 }
