5778b12f9fcb6367175fffd0ee763aa7fcf7ec26
seongho <tjdghgk98@gmail.com>
2023-08-02 01:39:21 +0900

    feat : node 서버에 이미지만 보내는 로직 초기 작성 -> node와 연동 후, 로직 수정 필요 -> 화요일까지 완성할 예정

--- backend/http/src/main/java/com/ssafy/http/apis/members/controllers/MemberPublicController.java
+++ backend/http/src/main/java/com/ssafy/http/apis/members/controllers/MemberPublicController.java
@@ -1,15 +1,28 @@
 package com.ssafy.http.apis.members.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.http.apis.members.services.MemberService;
+import com.ssafy.http.apis.members.services.S3ImageUploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
 public class MemberPublicController {
 
-    @GetMapping
-    public void test() {
+    private final MemberService memberService;
+    @PostMapping(value="/public/{government}/login", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String memberLogin(@PathVariable String government, @RequestParam(value="image") MultipartFile image) throws URISyntaxException {
 
+        System.out.println("로그인 요청 받음");
+        System.out.println(government);
+
+        String fileName = memberService.memberLogin(image, government);
+        return fileName;
     }
 }
--- backend/http/src/main/java/com/ssafy/http/apis/members/services/MemberService.java
+++ backend/http/src/main/java/com/ssafy/http/apis/members/services/MemberService.java
@@ -1,5 +1,52 @@
 package com.ssafy.http.apis.members.services;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+@Service
 public class MemberService {
 
+    public String memberLogin(MultipartFile faceImage, String government) throws URISyntaxException {
+
+        LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+
+        //String[] labels = new String[1];
+        //labels[0] = "lee";
+
+        //params.add("labels", labels);
+        params.add("face-image", faceImage);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<LinkedMultiValueMap<String, Object>> request
+                = new HttpEntity<>(params, headers);
+
+        String nodeUrl = "http://localhost:3000/check-face";
+
+        RestTemplate rest = new RestTemplate();
+
+        ResponseEntity<JsonNode> postForEntity
+                = rest.postForEntity(nodeUrl, request, JsonNode.class);
+
+        System.out.println(postForEntity.getStatusCodeValue());
+        System.out.println(postForEntity.getBody());
+        System.out.println(postForEntity.getHeaders());
+
+        return "hello";
+
+    }
 }
