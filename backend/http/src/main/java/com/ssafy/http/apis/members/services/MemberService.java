766c81fadad3d8614fec7acc64b4b3a3336ffdf4
seongho <tjdghgk98@gmail.com>
2023-08-02 22:28:22 +0900

    refactor : 로그인 처리 코드 이동(member -> auth)

--- backend/http/src/main/java/com/ssafy/http/apis/members/services/MemberService.java
+++ backend/http/src/main/java/com/ssafy/http/apis/members/services/MemberService.java
@@ -1,26 +1,19 @@
 package com.ssafy.http.apis.members.services;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.ssafy.http.apis.members.entities.MemberEntity;
 import com.ssafy.http.apis.members.repositories.MemberRepository;
+import com.ssafy.http.apis.members.requests.StudentRegisterRequest;
 import com.ssafy.http.apis.members.responses.StudentDetailResponse;
+import com.ssafy.http.exception.RegisterIdentificationException;
 import com.ssafy.http.exception.WrongParameterException;
 import com.ssafy.http.support.codes.ErrorCode;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -28,109 +21,61 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class MemberService {
 
-    private final MemberRepository memberRepository;
+  private final MemberRepository memberRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final S3ImageUploadService s3ImageUploadService;
 
-    public StudentDetailResponse getStudentDetail(Long studentId) {
-        StudentDetailResponse studentDetailResponse = new StudentDetailResponse();
+  @Value("${cloud.aws.s3.url}")
+  private String url;
 
-        studentDetailResponse.of(memberRepository.findById(studentId)
-                                                 .orElseThrow(
-                                                     () -> new WrongParameterException(
-                                                         ErrorCode.BAD_REQUEST_ERROR)));
+  private String imageType = ".png";
 
-        return studentDetailResponse;
+  public StudentDetailResponse getStudentDetail(Long studentId) {
+    StudentDetailResponse studentDetailResponse = new StudentDetailResponse();
+
+    studentDetailResponse.of(memberRepository.findById(studentId)
+        .orElseThrow(
+            () -> new WrongParameterException(
+                ErrorCode.BAD_REQUEST_ERROR)));
+
+    return studentDetailResponse;
+  }
+
+  public List<StudentDetailResponse> getClassStudents(Long classId) {
+    List<StudentDetailResponse> studentDetailResponses = new ArrayList<>();
+
+    List<MemberEntity> students = memberRepository.findAllByClassId(classId);
+
+    for (MemberEntity student : students) {
+      StudentDetailResponse studentDetailResponse = new StudentDetailResponse();
+
+      studentDetailResponse.of(student);
+
+      studentDetailResponses.add(studentDetailResponse);
     }
 
-    public List<StudentDetailResponse> getClassStudents(Long classId) {
-        List<StudentDetailResponse> studentDetailResponses = new ArrayList<>();
+    return studentDetailResponses;
+  }
 
-        List<MemberEntity> students = memberRepository.findAllByClassId(classId);
+  public void registerStudents(Long governmentId, MultipartFile faceImage,
+      StudentRegisterRequest studentRegisterRequest) {
 
-        for (MemberEntity student : students) {
-            StudentDetailResponse studentDetailResponse = new StudentDetailResponse();
+    MemberEntity memberEntity = studentRegisterRequest.toEntity(url, imageType, governmentId);
 
-            studentDetailResponse.of(student);
+    String folder = memberEntity.getGovernmentId().toString();
+    String uuid = memberEntity.getUuid();
 
-            studentDetailResponses.add(studentDetailResponse);
-        }
+    memberRepository.findMemberEntityByUuid(memberEntity.getUuid())
+        .ifPresent((student) -> new RegisterIdentificationException(
+            ErrorCode.ID_ALREADY_USE));
 
-        return studentDetailResponses;
+    try {
+      s3ImageUploadService.uploadImage(folder, uuid, imageType, faceImage);
+    } catch (IOException e) {
+      new RegisterIdentificationException(ErrorCode.IO_ERROR);
     }
 
-    public String memberLogin(MultipartFile faceImage, String government)
-        throws URISyntaxException, IOException {
-
-        File uploadFile = convert(faceImage)
-            .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
-
-        LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
-
-        String[] labels = new String[1];
-        labels[0] = "lee";
-
-        params.add("labels", labels);
-        //params.add("Face", faceImage);
-        params.add("Face", uploadFile);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-        HttpEntity<LinkedMultiValueMap<String, Object>> request
-            = new HttpEntity<>(params, headers);
-
-        String nodeUrl = "http://localhost:3000/check-face";
-
-        RestTemplate rest = new RestTemplate();
-
-        //이거 추가?
-        //rest.getMessageConverters().add(new ByteArrayHttpMessageConverter());
-
-        ResponseEntity<JsonNode> postForEntity
-            = rest.postForEntity(nodeUrl, request, JsonNode.class);
-
-        removeNewFile(uploadFile);
-
-        //System.out.println(postForEntity.getStatusCodeValue());
-
-        System.out.println(postForEntity.getBody()
-                                        .get("result")
-                                        .get(0)
-                                        .get("_label")
-                                        .asText());
-        System.out.println(postForEntity.getBody()
-                                        .get("result")
-                                        .get(0)
-                                        .get("_distance")
-                                        .asDouble());
-        //{"result":[{"_label":"lee","_distance":0.3462712649992261}]}
-        System.out.println(postForEntity.getHeaders());
-
-        return "hello";
-
-    }
-
-    private Optional<File> convert(MultipartFile file) throws IOException {
-
-        File convertFile = new File(file.getOriginalFilename());
-
-        if (convertFile.createNewFile()) {
-            try (FileOutputStream fos = new FileOutputStream(
-                convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
-                fos.write(file.getBytes());
-            }
-            return Optional.of(convertFile);
-        }
-
-        return Optional.empty();
-    }
-
-    private void removeNewFile(File targetFile) {
-        if (targetFile.delete()) {
-            //log.info("파일이 삭제되었습니다.");
-            System.out.println("파일 삭제되었습니다.");
-        } else {
-            //log.info("파일이 삭제되지 못했습니다.");
-            System.out.println("파일 삭제되지 않음");
-        }
-    }
+    memberEntity.encodePassword(passwordEncoder);
+    memberRepository.save(memberEntity);
+  }
 }
