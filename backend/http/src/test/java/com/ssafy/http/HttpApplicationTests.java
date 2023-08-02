2ff8ae1ba8998ab89324fee4dcf279d7ad6ed744
김정희 <spor1998@naver.com>
2023-08-02 14:41:44 +0900

    feat : http 배포

--- backend/http/src/test/java/com/ssafy/http/HttpApplicationTests.java
+++ backend/http/src/test/java/com/ssafy/http/HttpApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 //@SpringBootTest
 class HttpApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
