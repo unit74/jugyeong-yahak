ed27cb25611e941f72d7f2cd7fbf5518bb26d91a
seongho <tjdghgk98@gmail.com>
2023-08-02 11:56:29 +0900

    fix : members 컬럼 추가로 인한 entity fix

--- backend/http/src/main/java/com/ssafy/http/apis/members/entities/MemberEntity.java
+++ backend/http/src/main/java/com/ssafy/http/apis/members/entities/MemberEntity.java
@@ -21,75 +21,83 @@ import lombok.NoArgsConstructor;
 @Entity(name = "members")
 public class MemberEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false)
-    private Long governmentId;
+  @Column(nullable = false)
+  private Long governmentId;
 
-    @Column(nullable = true)
-    private Long classId;
+  @Column(nullable = true)
+  private Long classId;
 
-    @ManyToOne
-    @JoinColumn(name = "roles", nullable = false)
-    private RoleEntity role;
+  @ManyToOne
+  @JoinColumn(name = "roles", nullable = false)
+  private RoleEntity role;
 
-    @Column(nullable = false)
-    private String statusCode;
+  @Column(nullable = false)
+  private String statusCode;
 
-    @Column(nullable = false)
-    private String name;
+  @Column(nullable = false)
+  private String uuid;
 
-    @Column(nullable = false)
-    private String phone;
+  @Column(nullable = false)
+  private String password;
 
-    @Column(nullable = false)
-    private String address;
+  @Column(nullable = false)
+  private String name;
 
-    @Column(nullable = false)
-    private String faceImageUrl;
+  @Column(nullable = false)
+  private String phone;
 
-    @Column(nullable = false)
-    private String firstResponder;
+  @Column(nullable = false)
+  private String address;
 
-    @Column(nullable = true)
-    private Long tabletNo;
+  @Column(nullable = false)
+  private String faceImageUrl;
 
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
+  @Column(nullable = false)
+  private String firstResponder;
 
-    @Column(nullable = false)
-    private LocalDateTime updatedAt;
+  @Column(nullable = true)
+  private Long tabletNo;
 
-    @Builder
-    public MemberEntity(Long id, Long governmentId, Long classId, RoleEntity role,
-        String statusCode,
-        String name, String phone, String address, String faceImageUrl, String firstResponder,
-        Long tabletNo, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.governmentId = governmentId;
-        this.classId = classId;
-        this.role = role;
-        this.statusCode = statusCode;
-        this.name = name;
-        this.phone = phone;
-        this.address = address;
-        this.faceImageUrl = faceImageUrl;
-        this.firstResponder = firstResponder;
-        this.tabletNo = tabletNo;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
+  @Column(nullable = false)
+  private LocalDateTime createdAt;
 
-    @PrePersist
-    public void createTimeStamps() {
-        updatedAt = LocalDateTime.now();
-        createdAt = LocalDateTime.now();
-    }
+  @Column(nullable = false)
+  private LocalDateTime updatedAt;
 
-    @PreUpdate
-    public void updateTimeStamps() {
-        updatedAt = LocalDateTime.now();
-    }
+  public MemberEntity(Long id, Long governmentId, Long classId, RoleEntity role, String statusCode,
+      String uuid, String password, String name, String phone, String address, String faceImageUrl,
+      String firstResponder, Long tabletNo, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    this.id = id;
+    this.governmentId = governmentId;
+    this.classId = classId;
+    this.role = role;
+    this.statusCode = statusCode;
+    this.uuid = uuid;
+    this.password = password;
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+    this.faceImageUrl = faceImageUrl;
+    this.firstResponder = firstResponder;
+    this.tabletNo = tabletNo;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  @Builder
+
+  @PrePersist
+  public void createTimeStamps() {
+    updatedAt = LocalDateTime.now();
+    createdAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void updateTimeStamps() {
+    updatedAt = LocalDateTime.now();
+  }
 }
