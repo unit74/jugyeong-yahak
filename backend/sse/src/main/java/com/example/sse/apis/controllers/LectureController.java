a23b57b7fca252fc32988b119debd7d084c99d8d
unit74 <u__nit@naver.com>
2023-08-02 22:34:07 +0900

    feat: 마우스 이동에 대한 API 작성

--- backend/sse/src/main/java/com/example/sse/apis/controllers/LectureController.java
+++ backend/sse/src/main/java/com/example/sse/apis/controllers/LectureController.java
@@ -1,10 +1,13 @@
 package com.example.sse.apis.controllers;
 
+import com.example.sse.apis.requests.MoveMouseRequest;
 import com.example.sse.apis.services.LectureService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -21,8 +24,14 @@ public class LectureController {
         return lectureService.subscribe(teacherId);
     }
 
-    @GetMapping("/convert/page/{number}")
-    public void sendData(@PathVariable Long number) {
-        lectureService.convertPage(1L, number);
+    @PostMapping("/convert/page/{teacherId}/{number}")
+    public void convertPage(@PathVariable Long teacherId, @PathVariable Long number) {
+        lectureService.convertPage(teacherId, number);
+    }
+
+    @PostMapping("/mouse/pointer/{teacherId}")
+    public void moveMouseCursor(@PathVariable Long teacherId,
+        @RequestBody MoveMouseRequest moveMouseRequest) {
+        lectureService.moveMousePointer(teacherId, moveMouseRequest);
     }
 }
---
+++ backend/sse/src/main/java/com/example/sse/apis/requests/MoveMouseRequest.java
@@ -0,0 +1,14 @@
+package com.example.sse.apis.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MoveMouseRequest {
+
+    Long x;
+    Long y;
+}
