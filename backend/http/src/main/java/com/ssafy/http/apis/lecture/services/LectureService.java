7e981258189f97c532e9b22b2769b0f94c844f14
unit74 <u__nit@naver.com>
2023-08-03 22:51:16 +0900

    feat: SSE 서버에게 요청하는 API 작성

--- backend/http/src/main/java/com/ssafy/http/apis/lecture/services/LectureService.java
+++ backend/http/src/main/java/com/ssafy/http/apis/lecture/services/LectureService.java
@@ -1,31 +1,58 @@
 package com.ssafy.http.apis.lecture.services;
 
-import com.ssafy.http.apis.members.services.MemberService;
+import com.ssafy.http.apis.lecture.requests.ControlMicRequest;
+import com.ssafy.http.apis.lecture.requests.ConvertPageRequest;
+import com.ssafy.http.apis.lecture.requests.MoveMouseRequest;
 import java.net.URI;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
-@RequiredArgsConstructor
-public class LectrueService {
+public class LectureService {
 
-    private final MemberService memberService;
+    @Value("${sse.server.url}")
+    private String BASE_URL;
 
-    public SseEmitter startLecture(Long id) {
-        Long classId = memberService.getClassId(id);
+    public void controlMic(ControlMicRequest controlMicRequest) {
+        RestTemplate restTemplate = new RestTemplate();
 
-        URI uri = UriComponentsBuilder.fromUriString("http://localhost:8082")
-                                      .path("/api/v1/sse/subscribe")
-                                      .path("/" + classId)
+        HttpEntity<ControlMicRequest> request = new HttpEntity<>(controlMicRequest);
+
+        URI uri = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+                                      .path("/convert/page")
                                       .build()
                                       .toUri();
 
+        restTemplate.postForLocation(uri, request);
+    }
+
+    public void moveMousePointer(MoveMouseRequest moveMouseRequest) {
         RestTemplate restTemplate = new RestTemplate();
 
-        return null;
-//        return restTemplate.postForEntity(uri, SseEmitter.class);
+        HttpEntity<MoveMouseRequest> request = new HttpEntity<>(moveMouseRequest);
+
+        URI uri = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+                                      .path("/mouse/pointer")
+                                      .build()
+                                      .toUri();
+
+        restTemplate.postForLocation(uri, request);
     }
+
+    public void convertPage(ConvertPageRequest convertPageRequest) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<ConvertPageRequest> request = new HttpEntity<>(convertPageRequest);
+
+        URI uri = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+                                      .path("/mic/control")
+                                      .build()
+                                      .toUri();
+
+        restTemplate.postForLocation(uri, request);
+    }
+
 }
