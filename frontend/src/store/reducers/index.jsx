46dec70574e776cf952a108252d300c6d4482860
nayeon <kny0628m@gmail.com>
2023-08-03 14:29:28 +0900

    feat : 컴포넌트 구조와 Redux 대공사 중입니다

--- frontend/src/store/reducers/index.jsx
+++ frontend/src/store/reducers/index.jsx
@@ -3,12 +3,16 @@
 import { combineReducers } from 'redux';
 import allThemesReducer from './allThemesReducer';
 import themeReducer from './themeReducer';
-import wordsListReducer from './wordsListReducer';
+import setSelectedThemeReducer from './setSelectedThemeReducer';
+import setWordIndexReducer from './setWordIndexReducer';
+
 
 const rootReducer = combineReducers({
   allThemesState: allThemesReducer,
   themeState : themeReducer,
-  wordsListState : wordsListReducer
+  // 교사가 carousel에서 커리 선택하면 여기에 저장됨.
+  selectedThemeState : setSelectedThemeReducer,
+  wordIndexState : setWordIndexReducer
 });
 
 export default rootReducer;
