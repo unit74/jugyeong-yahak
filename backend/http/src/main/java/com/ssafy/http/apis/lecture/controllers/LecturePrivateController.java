93092eedd451dd7ec85bdd60ba685f574f2135bf
unit74 <u__nit@naver.com>
2023-08-03 17:53:36 +0900

    feat: 1차 개발 중이었지만, SSE 통신 부분 고쳐야할 것 같아서 잠시 push

--- backend/http/src/main/java/com/ssafy/http/apis/lecture/controllers/LecturePrivateController.java
+++ backend/http/src/main/java/com/ssafy/http/apis/lecture/controllers/LecturePrivateController.java
@@ -1,8 +1,13 @@
 package com.ssafy.http.apis.lecture.controllers;
 
+import com.ssafy.http.apis.lecture.requests.MicControlRequest;
+import com.ssafy.http.apis.lecture.requests.MousePointerRequest;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/private/lecture")
 public class LecturePrivateController {
 
-    @GetMapping(value = "/start", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<?> startLecture() {
-        return null;
+    @GetMapping("/convert/page/{number}")
+    public void convertPage(@PathVariable Long number) {
+    }
+
+    @PostMapping("/mouse/pointer")
+    public void moveMouseCursor(@RequestBody MousePointerRequest mousePointerRequest) {
+    }
+
+    @PostMapping("/mic/control")
+    public void micControl(@RequestBody MicControlRequest micControlRequest) {
+
+
     }
 }
