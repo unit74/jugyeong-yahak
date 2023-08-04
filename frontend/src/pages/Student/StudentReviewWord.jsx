d8d01fb497d854e1f2c43f7fa19a65e28aa8aec4
nayeon <kny0628m@gmail.com>
2023-08-04 11:27:18 +0900

    feat : review-word 컴포넌트에 잘못들어간 녹음 로직 제거

--- frontend/src/pages/Student/StudentReviewWord.jsx
+++ frontend/src/pages/Student/StudentReviewWord.jsx
@@ -1,66 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./StudentReviewWord.module.css";
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from "react-speech-recognition";
 
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTheme } from "../../store/actions/themeAction";
 
-import { useDebounce } from "../Common/hooks/useDebounce";
 
 export default function StudentReviewWord() {
-  // axios !!!!!!!!!
-  // 단어 조회
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const wordsList = useSelector((state) => state.themeState.wordsList) || [];
   const wordIndex = useSelector((state) => state.wordIndexState.wordIndex);
 
-  const {
-    transcript, // 말이 변환된 글자!!!!!!!
-    listening,
-    // resetTranscript,
-    browserSupportsSpeechRecognition,
-  } = useSpeechRecognition();
-
-  const [speechWord, setSpeechWord] = useState("");
-  const navigate = useNavigate();
-  const debounceTerm = useDebounce(speechWord, 3000); // speechWord가 끝나면 3초 후에 정답 처리를 위해
-  // 오답처리나 정답 처리를 바로 하지 않기 위해서
-
   useEffect(() => {
-    dispatch(fetchTheme());
-    const timer = setTimeout(() => {
-      SpeechRecognition.startListening();
-      // console.log("마운트 5초뒤 speech 함수가 실행되었습니다.");
-    }, 800); // 800ms = 0.8초  노인 반응 속도논문 평균 0.846초이니까 먼저 녹음 시작
-    // 5초 동안 녹음 지속
-
-    // 컴포넌트가 언마운트될 때 타이머를 정리합니다.
-    return () => clearTimeout(timer);
-  }, []); 
-
-  useEffect(() => {
-    setSpeechWord(transcript);
-  }, [transcript]); // transcript가 변경되면 speechWord가 state 변경시킨다.
-
-  useEffect(() => {
-
     dispatch(fetchTheme());
     const timer = setTimeout(() => {
       navigate("/record-word");
-    }, 10000); // 10초
+    }, 10000); // 10초 후 전환
 
     // 언마운트 됐을시 타이머 클리어
     return () => {
-      // clearTimeout(timer);
+      clearTimeout(timer);
     };
   }, [navigate]);
-
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
 
   return (
     <div className={styles.main}>
