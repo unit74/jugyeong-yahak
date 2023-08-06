8dff8d501e39c1b1c4eead66316887e98b292613
seongho <tjdghgk98@gmail.com>
2023-08-07 01:39:03 +0900

    branch recover
    Former-commit-id: 2f8bc5349bcc648cd7e40c399025cc4cf1704910

--- backend/http/src/main/java/com/ssafy/http/apis/classes/services/ClassService.java
+++ backend/http/src/main/java/com/ssafy/http/apis/classes/services/ClassService.java
@@ -3,38 +3,48 @@ package com.ssafy.http.apis.classes.services;
 import com.ssafy.http.apis.classes.entities.ClassEntity;
 import com.ssafy.http.apis.classes.repositories.ClassRepository;
 import com.ssafy.http.apis.classes.responses.ClassDetailResponse;
-import com.ssafy.http.apis.members.entities.MemberEntity;
-import com.ssafy.http.apis.members.repositories.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ClassService {
 
-    private final ClassRepository classRepository;
+  private final ClassRepository classRepository;
 
-    public List<ClassDetailResponse> getClassList(Long governmentId) {
+  public List<ClassDetailResponse> getClassList(Long governmentId) {
 
-        List<ClassDetailResponse> classDetailResponses = new ArrayList<>();
+    List<ClassDetailResponse> classDetailResponses = new ArrayList<>();
 
-        List<ClassEntity> classes = classRepository.findAllByGovernmentId(governmentId);
+    List<ClassEntity> classes = classRepository.findAllByGovernmentId(governmentId);
 
-        for(ClassEntity class_ : classes ) {
-            ClassDetailResponse classDetailResponse = new ClassDetailResponse();
+    for (ClassEntity class_ : classes) {
+      ClassDetailResponse classDetailResponse = new ClassDetailResponse();
 
-            classDetailResponse.of(class_);
+      classDetailResponse.of(class_);
 
-            classDetailResponses.add(classDetailResponse);
-        }
-
-        return classDetailResponses;
+      classDetailResponses.add(classDetailResponse);
     }
 
+    return classDetailResponses;
+  }
+
+  public List<ClassDetailResponse> getGovernmentClassList(Long governmentId) {
+
+    List<ClassDetailResponse> classDetailResponses = new ArrayList<>();
+
+    List<ClassEntity> classEntities = classRepository.findAllByGovernmentId(governmentId);
+
+    for (ClassEntity entity : classEntities) {
+      ClassDetailResponse response = new ClassDetailResponse();
+      response.of(entity);
+      classDetailResponses.add(response);
+    }
+
+    return classDetailResponses;
+  }
 
 
 }
