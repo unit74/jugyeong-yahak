5a59c2f41d3c38b09ff477e705ac8f642f70a90c
nayeon <kny0628m@gmail.com>
2023-08-07 09:00:43 +0900

    to merge, nothing changed

--- frontend/src/pages/Student/GoodFeedback.jsx
+++ frontend/src/pages/Student/GoodFeedback.jsx
@@ -5,30 +5,29 @@ import good from "../../assets/images/good_feedback.png";
 import confetti from "canvas-confetti";
 
 import { useDispatch, useSelector } from "react-redux";
-import { setWordIndex } from '../../store/actions/setWordIndexAction';
+import { setWordIndex } from "../../store/actions/setWordIndexAction";
 
 //단어읽기 문제 표시 페이지
 export default function GoodFeedback() {
   const dispatch = useDispatch();
   const location = useLocation();
-  const course = location.state.course;
+  const course = (location.state && location.state.course) || "";
   const navigate = useNavigate();
-  
-  
+
   const wordIndex = useSelector((state) => state.wordIndexState.wordIndex);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (course === 'reading') {
-        navigate('/dictation-main');
-      } else if (course === 'writing' && wordIndex < 5) {
+      if (course === "reading") {
+        navigate("/dictation-main");
+      } else if (course === "writing" && wordIndex < 5) {
         dispatch(setWordIndex());
-        navigate('/review-word');
-      } else if (course === 'writing' && wordIndex === 5) {
-        navigate('/diary');
-      } else if (course === 'diary') {
-        navigate('/student-done');
-      } 
+        navigate("/review-word");
+      } else if (course === "writing" && wordIndex === 5) {
+        navigate("/diary");
+      } else if (course === "diary") {
+        navigate("/student-done");
+      }
     }, 5000); // 10초
 
     // 언마운트 될 시 타이머 클리어
@@ -39,7 +38,7 @@ export default function GoodFeedback() {
 
   // 컨페티효과
   confetti({
-    spread: 360,
+    spread: 400,
     particleCount: 400,
     angle: 360,
   });
--- frontend/src/store/actions/setWordIndexAction.jsx
+++ frontend/src/store/actions/setWordIndexAction.jsx
@@ -1,6 +1,7 @@
 export const SET_WORD_INDEX = 'SET_WORD_INDEX';
-export const setWordIndex = () => {
+export const setWordIndex = (reset = false) => {
   return {
     type: SET_WORD_INDEX,
+    payload: reset,
   };
 };
