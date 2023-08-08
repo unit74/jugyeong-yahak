1313295a3583ac13711801c0bc5292050df6bcaf
spor1998 <spor1998@naver.com>
2023-08-09 01:55:57 +0900

    feat : book ok

--- frontend/src/pages/Student/StudentBook.jsx
+++ frontend/src/pages/Student/StudentBook.jsx
@@ -9,12 +9,6 @@ export default function StudentBook() {
   const [fade, setFade] = useState(false);
   const [book, setBook] = useState([]);
 
-  book.push({ theme: "병원", status: 5 });
-  book.push({ theme: "시장", status: 2 });
-  book.push({ theme: "학교", status: 0 });
-  book.push({ theme: "학원", status: 4 });
-  book.push({ theme: "정희", status: 5 });
-
   const bookAnimationClassNames = {
     appearActive: styles.bounceIn,
   };
@@ -23,12 +17,20 @@ export default function StudentBook() {
     transform: "perspective(1000px) rotateY(-17deg)",
   };
 
-  // useEffect(() => {
-  //   axios
-  //     .get("https://i9e206.p.ssafy.io/api/v1/themes/8")
-  //     .then((response) => {})
-  //     .catch((error) => console.error(`Error: ${error}`));
-  // }, []);
+  useEffect(() => {
+    // 일단 인증정보 그냥 넣어둠
+    axios
+      .get("https://i9e206.p.ssafy.io/api/v1/members/libraries", {
+        headers: {
+          Authorization:
+            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2OTE1MTE0NTgsInN1YiI6ImFjY2Vzcy10b2tlbiIsImlkZW50aWZpY2F0aW9uIjoiMTIiLCJyb2xlIjoiUk9MRV9TVFVERU5UIn0.jpy8sqMLP4VEbD0AOYqC0pNG6aLNxeygDASyOi03cnBRxt7U0Fy2kARB2bsm81U0EbUe5unQOqlkod-D6rbdBQ",
+        },
+      })
+      .then((response) => {
+        setBook(response.data.data.list);
+      })
+      .catch((error) => console.error(`Error: ${error}`));
+  }, []);
 
   return (
     <div className={`${styles.main} ${fade ? styles.fadeOut : ""}`}>
@@ -37,8 +39,12 @@ export default function StudentBook() {
           <b className={styles.greeting}>현재 공부</b>
           {book.map((sub, i) => (
             <CSSTransition in={true} appear timeout={5000} classNames={bookAnimationClassNames}>
-              <div className={`${styles.book} ${styles[`book${i + 1}`]}`} style={bookStyle}>
-                {sub.theme + "  :  "}
+              <div
+                id={`book${i + 1}`}
+                className={`${styles.book} ${styles[`book${i + 1}`]}`}
+                style={bookStyle}
+              >
+                {sub.themeName + "  :  "}
                 {sub.status === 0 ? "시작 안함" : sub.status === 5 ? "완료됨" : "공부 중"}
               </div>
             </CSSTransition>
