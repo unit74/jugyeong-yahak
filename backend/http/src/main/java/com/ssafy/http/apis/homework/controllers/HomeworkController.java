17f8a5743436b7c1a12a07189ff5ad8ad3c2a259
seongho <tjdghgk98@gmail.com>
2023-08-09 00:01:28 +0900

    add : 책장 불러오는 API 작성 완료
    Former-commit-id: 3407eff6075be6c420f8b6d5e90f338a6c57a0ac

---
+++ backend/http/src/main/java/com/ssafy/http/apis/homework/controllers/HomeworkController.java
@@ -0,0 +1,33 @@
+package com.ssafy.http.apis.homework.controllers;
+
+import static com.ssafy.http.support.utils.ApiResponseUtil.createSuccessResponse;
+
+import com.ssafy.http.apis.homework.responses.LibraryResponse;
+import com.ssafy.http.apis.homework.services.HomeworkService;
+import com.ssafy.http.security.utils.SecurityUtil;
+import com.ssafy.http.support.codes.SuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members/libraries")
+public class HomeworkController {
+
+  private final HomeworkService homeworkService;
+
+  @GetMapping //추후에 private
+  public ResponseEntity<?> getLibraryList() { //학생이 getLibraryList() 호출함
+
+    LibraryResponse result = homeworkService.getLibraryList(SecurityUtil.getLoginUserId());
+
+    return createSuccessResponse(SuccessCode.SELECT_SUCCESS,
+        "책장 정보를 불러왔습니다.",
+        result);
+
+  }
+
+}
