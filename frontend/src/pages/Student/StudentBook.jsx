e4caed6feed6be79df6b97541b501b3ef9ce5e4d
Yejin Kim <yeajin7933@gmail.com>
2023-08-08 16:55:22 +0900

    Edit StudentBook

--- frontend/src/pages/Student/StudentBook.jsx
+++ frontend/src/pages/Student/StudentBook.jsx
@@ -1,31 +1,80 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import styles from "./StudentBook.module.css";
-import { useLocation, useNavigate } from "react-router-dom";
-import useTimeoutCallback from "../Common/hooks/useTimeoutCallback";
+import { useNavigate } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 
 export default function StudentBook() {
   const navigate = useNavigate();
   const [fade, setFade] = useState(false);
 
-  // const navigateToRecordDictation = useCallback(() => {
-  //   setFade(true);
-  //   setTimeout(() => {
-  //     navigate("/");
-  //   }, 1000); // fadeout 후 이동
-  // }, [navigate]);
+  const bookAnimationClassNames = {
+    appearActive: styles.bounceIn,
+  };
 
-  // useTimeoutCallback(navigateToRecordDictation, 15000); // 10초
+  const bookStyle = {
+    transform: "perspective(1000px) rotateY(-17deg)",
+  };
 
   return (
     <div className={`${styles.main} ${fade ? styles.fadeOut : ""}`}>
       <div className={styles.square}>
         <div className={styles.books}>
           <b className={styles.greeting}>현재 공부</b>
-          <div className={`${styles.book} ${styles.book1}`} />
-          <div className={`${styles.book} ${styles.book2}`} />
-          <div className={`${styles.book} ${styles.book3}`} />
-          <div className={`${styles.book} ${styles.book4}`} />
-          <div className={`${styles.book} ${styles.book5}`} />
+          <CSSTransition
+            in={true}
+            appear
+            timeout={5000}
+            classNames={bookAnimationClassNames}
+          >
+            <div
+              className={`${styles.book} ${styles.book1}`}
+              style={bookStyle}
+            />
+          </CSSTransition>
+          <CSSTransition
+            in={true}
+            appear
+            timeout={5000}
+            classNames={bookAnimationClassNames}
+          >
+            <div
+              className={`${styles.book} ${styles.book2}`}
+              style={bookStyle}
+            />
+          </CSSTransition>
+          <CSSTransition
+            in={true}
+            appear
+            timeout={5000}
+            classNames={bookAnimationClassNames}
+          >
+            <div
+              className={`${styles.book} ${styles.book3}`}
+              style={bookStyle}
+            />
+          </CSSTransition>
+          <CSSTransition
+            in={true}
+            appear
+            timeout={5000}
+            classNames={bookAnimationClassNames}
+          >
+            <div
+              className={`${styles.book} ${styles.book4}`}
+              style={bookStyle}
+            />
+          </CSSTransition>
+          <CSSTransition
+            in={true}
+            appear
+            timeout={5000}
+            classNames={bookAnimationClassNames}
+          >
+            <div
+              className={`${styles.book} ${styles.book5}`}
+              style={bookStyle}
+            />
+          </CSSTransition>
         </div>
       </div>
     </div>
