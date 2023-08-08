d8961b31dffe2272cea23b1c8d494b0b52a63787
seongho <tjdghgk98@gmail.com>
2023-08-09 03:00:50 +0900

    fix : gender를 응답에 포함시킴
    Former-commit-id: 07e916d6f0fcfbbb3d33d54134bc11d06e58dade

--- backend/http/src/main/java/com/ssafy/http/apis/members/responses/StudentDetailResponse.java
+++ backend/http/src/main/java/com/ssafy/http/apis/members/responses/StudentDetailResponse.java
@@ -13,34 +13,36 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class StudentDetailResponse {
 
-    private Long id;
-    private Long governmentId;
-    private Long classId;
-    private String role;
-    private String statusCode;
-    private String name;
-    private String phone;
-    private String address;
-    private String faceImageUrl;
-    private String firstResponder;
-    private Long tabletNo;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+  private Long id;
+  private Long governmentId;
+  private Long classId;
+  private String role;
+  private String statusCode;
+  private String name;
+  private String phone;
+  private String address;
+  private String faceImageUrl;
+  private String firstResponder;
+  private Long tabletNo;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+  private String gender;
 
-    public void of(MemberEntity memberEntity) {
-        this.id = memberEntity.getId();
-        this.governmentId = memberEntity.getGovernmentId();
-        this.classId = memberEntity.getClassId();
-        this.role = memberEntity.getRole()
-                                .getRole();
-        this.statusCode = memberEntity.getStatusCode();
-        this.name = memberEntity.getName();
-        this.phone = memberEntity.getPhone();
-        this.address = memberEntity.getAddress();
-        this.faceImageUrl = memberEntity.getFaceImageUrl();
-        this.firstResponder = memberEntity.getFirstResponder();
-        this.tabletNo = memberEntity.getTabletNo();
-        this.createdAt = memberEntity.getCreatedAt();
-        this.updatedAt = memberEntity.getUpdatedAt();
-    }
+  public void of(MemberEntity memberEntity) {
+    this.id = memberEntity.getId();
+    this.governmentId = memberEntity.getGovernmentId();
+    this.classId = memberEntity.getClassId();
+    this.role = memberEntity.getRole()
+        .getRole();
+    this.statusCode = memberEntity.getStatusCode();
+    this.name = memberEntity.getName();
+    this.phone = memberEntity.getPhone();
+    this.address = memberEntity.getAddress();
+    this.faceImageUrl = memberEntity.getFaceImageUrl();
+    this.firstResponder = memberEntity.getFirstResponder();
+    this.tabletNo = memberEntity.getTabletNo();
+    this.createdAt = memberEntity.getCreatedAt();
+    this.updatedAt = memberEntity.getUpdatedAt();
+    this.gender = (memberEntity.getGender() == 1) ? "MAN" : "WOMAN";
+  }
 }
