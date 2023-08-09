c487e3c4c4ceae88c6bda6a05a01cd713153a8e4
spor1998 <spor1998@naver.com>
2023-08-09 22:00:44 +0900

    feat : TTS 1차 완료

--- frontend/src/pages/Student/StudentDiary.jsx
+++ frontend/src/pages/Student/StudentDiary.jsx
@@ -5,7 +5,8 @@ import useTimeoutCallback from "../Common/hooks/useTimeoutCallback";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTheme } from "../../store/actions/themeAction";
 import friends from "../../assets/images/friends.png";
-import { Configuration, OpenAIApi } from "openai";;
+import { Configuration, OpenAIApi } from "openai";
+import TTS from "../Common/TTS";
 
 // 예시영상 페이지
 export default function StudentDiary() {
@@ -14,38 +15,49 @@ export default function StudentDiary() {
   const themeData = useSelector((state) => state.themeState.themeData) || {};
   const navigate = useNavigate();
   const location = useLocation();
-  const message = (location.state && location.state.message);
-  const [isGenerating, setIsGenerating] = useState(false); 
-  const [generatedText, setGeneratedText] = useState('');
+  const message = location.state && location.state.message;
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [generatedText, setGeneratedText] = useState("");
+
+  const [msg, setMsg] = useState(null);
 
   // 온점일 때 줄 띄우기
   const formattedDiary =
     (themeData &&
       themeData.situationJournal &&
-      themeData.situationJournal.split(". ").join(".\n")) || "";
-      
-  const formattedText =
-    (generatedText &&
-      generatedText&&
-      generatedText.split(". ").join(".\n")) || "";
+      themeData.situationJournal.split(". ").join(".\n")) ||
+    "";
 
+  const formattedText =
+    (generatedText && generatedText && generatedText.split(". ").join(".\n")) || "";
 
   const navigateToRecordDictation = useCallback((navigate) => {
-    navigate("/good-feedback", { state: { course: "diary" } }); 
+    navigate("/good-feedback", { state: { course: "diary" } });
   }, []);
 
-  useTimeoutCallback(navigateToRecordDictation, 10000); 
+  const ttsMaker = async (msg, timer) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        setMsg(msg);
+        resolve();
+      }, timer);
+    });
+  };
+
+  useTimeoutCallback(navigateToRecordDictation, 10000);
 
   useEffect(() => {
-    if (message === '') {
+    if (message === "") {
       dispatch(fetchTheme());
     } else {
-      generateText()
+      generateText();
     }
-    return () => {
-    };
+    return () => {};
   }, [message, dispatch]);
 
+  useEffect(() => {
+    ttsMaker("완성된 일기를 한 문장씩 따라 읽어요!!", 0);
+  }, []);
 
   //함수
   // 1. API요청 함수
@@ -54,7 +66,7 @@ export default function StudentDiary() {
       setIsGenerating(true);
 
       try {
-        const apiKey = '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';
+        const apiKey = "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";
 
         const configuration = new Configuration({
           apiKey: apiKey,
@@ -64,21 +76,23 @@ export default function StudentDiary() {
         const response = await openai.createChatCompletion({
           model: "gpt-3.5-turbo",
           messages: [
-            {"role": "system", "content": "70대가 쓴 일기처럼 작성해줘."},
-            {role: "user", content: `다음 내용을 짧은 4개의 문장으로 일기처럼 작성해줘 : ${message}`}
+            { role: "system", content: "70대가 쓴 일기처럼 작성해줘." },
+            {
+              role: "user",
+              content: `다음 내용을 짧은 4개의 문장으로 일기처럼 작성해줘 : ${message}`,
+            },
           ],
         });
 
         const generatedMessage = response.data.choices[0].message.content;
         setGeneratedText(generatedMessage);
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
       } finally {
         setIsGenerating(false);
       }
     }
   };
-
 
   return (
     <div className={styles.main}>
@@ -87,6 +101,7 @@ export default function StudentDiary() {
           {/* <img src={friends} alt="friends_img" /> */}
           <b className={styles.diarytext}>{formattedDiary}</b>
           <b className={styles.diarytext}>{formattedText}</b>
+          {msg && <TTS message={msg} />}
         </div>
       </div>
     </div>
