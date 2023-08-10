a0c8c852b1e54c36032c2ae35c5faa8504e8453a
seongho <tjdghgk98@gmail.com>
2023-08-11 02:14:27 +0900

    feat : 클 : 테마 선택 => 테마에 대한 컨텐츠 list 요청을 처리
    Former-commit-id: 747bef16e07cc4494b5fdf06cac07f22ec031800

--- backend/http/src/main/java/com/ssafy/http/apis/lecturehistories/repositories/LectureHistoryRepository.java
+++ backend/http/src/main/java/com/ssafy/http/apis/lecturehistories/repositories/LectureHistoryRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.http.apis.lecturehistories.repositories;
 
 import com.ssafy.http.apis.classes.entities.ClassEntity;
+import com.ssafy.http.apis.themes.entities.ThemeEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,12 @@ public interface LectureHistoryRepository extends JpaRepository<ClassEntity, Lon
       + "ON lh.classId = :classId AND t.id = lh.themeEntity.id "
       + "GROUP BY t.theme")
   List<Object[]> findThemeNameAllForClass(@Param("classId") Long classId);
+
+  @Query("SELECT t FROM LectureHistoryEntity lh "
+      + "RIGHT JOIN lh.themeEntity t "
+      + "ON lh.classId = :classId AND t.id = lh.themeEntity.id "
+      + "WHERE t.theme = :themeName AND lh.id IS NULL")
+  List<ThemeEntity> findStagesAllForClass(@Param("themeName") String themeName,
+      @Param("classId") Long classId);
+
 }
--- backend/http/src/main/java/com/ssafy/http/apis/themes/requests/StagesRequest.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.http.apis.themes.requests;
-
-public class StagesRequest {
-
-  //테마명 string 이 필요함
-  String themeName;
-}
