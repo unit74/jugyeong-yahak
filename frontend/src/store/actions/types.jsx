35e6358adfdf3ce72b30dc0c07953f84d78f7bd1
nayeon <kny0628m@gmail.com>
2023-08-10 23:05:38 +0900

    feat : 메인 다음 공책유무 선택 컴포넌트 추가. 받아쓰기에서 공책O -> 웹캠, 공책X -> 필기나오게 함

--- frontend/src/store/actions/types.jsx
+++ frontend/src/store/actions/types.jsx
@@ -5,3 +5,4 @@ export const FETCH_THEME_SUCCESS = "FETCH_THEME_SUCCESS";
 export const SET_SELECTED_THEME = "SET_SELECTED_THEME";
 export const SET_WORD_INDEX = "SET_WORD_INDEX";
 export const SET_MESSAGE = "SET_MESSAGE";
+export const SET_NOTE = "SET_NOTE";
