9c5ed03450d8a8a4446c0d991067224a0d13f867
unit74 <u__nit@naver.com>
2023-08-10 21:16:29 +0900

    refactor: 비디오 크기 조정

--- frontend/src/components/OvVideo.js
+++ frontend/src/components/OvVideo.js
@@ -1,25 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-export default class OpenViduVideoComponent extends Component {
+export default class OvVideoComponent extends Component {
   constructor(props) {
     super(props);
-    this.props = props;
     this.videoRef = React.createRef();
+  }
+
+  componentDidMount() {
+    if (this.props && this.props.user.streamManager && !!this.videoRef) {
+      console.log('PROPS: ', this.props);
+      this.props.user.getStreamManager().addVideoElement(this.videoRef.current);
+    }
   }
 
   componentDidUpdate(props) {
     if (props && !!this.videoRef) {
-      this.props.streamManager.addVideoElement(this.videoRef.current);
-    }
-  }
-
-  componentDidMount() {
-    if (this.props && !!this.videoRef) {
-      this.props.streamManager.addVideoElement(this.videoRef.current);
+      this.props.user.getStreamManager().addVideoElement(this.videoRef.current);
     }
   }
 
   render() {
-    return <video autoPlay={true} ref={this.videoRef} />;
+    return (
+      <video
+        autoPlay={true}
+        style={{ display: 'block', width: '100%', height: '100%' }}
+        id={'video-' + this.props.user.getStreamManager().stream.streamId}
+        ref={this.videoRef}
+      />
+    );
   }
 }
