8dc8501375ef59b4a1ece90f0e3dbcdb88739ec1
seongho <tjdghgk98@gmail.com>
2023-08-12 22:04:13 +0900

    feat : diary 등록하는 API 작성 완료 V1 - 이미지 부분은 고려하지 않았음
    Former-commit-id: 8fcb05a07b0d2e0c41b9aef315c0b163de2240f2

--- backend/http/src/main/java/com/ssafy/http/apis/diaries/controllers/DiaryPrivateController.java
+++ backend/http/src/main/java/com/ssafy/http/apis/diaries/controllers/DiaryPrivateController.java
@@ -3,6 +3,7 @@ package com.ssafy.http.apis.diaries.controllers;
 
 import static com.ssafy.http.support.utils.ApiResponseUtil.createSuccessResponse;
 
+import com.ssafy.http.apis.diaries.requests.DiaryRegisterRequest;
 import com.ssafy.http.apis.diaries.responses.DiaryDetailResponse;
 import com.ssafy.http.apis.diaries.services.DiaryService;
 import com.ssafy.http.security.utils.SecurityUtil;
@@ -12,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +43,13 @@ public class DiaryPrivateController {
 
     return createSuccessResponse(SuccessCode.SELECT_SUCCESS, "일기 리스트를 조회하였습니다.",
         diaryDetailResponses);
+  }
+
+  @PostMapping
+  public ResponseEntity registerDiary(@RequestBody DiaryRegisterRequest diaryRegisterRequest) {
+
+    diaryService.registerDiary(SecurityUtil.getLoginUserId(), diaryRegisterRequest);
+
+    return createSuccessResponse(SuccessCode.INSERT_SUCCESS, "일기 등록을 성공했습니다.");
   }
 }
