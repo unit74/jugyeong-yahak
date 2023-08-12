8b75f68183f55ac8336aef684ef548b6722f2dbf
Yejin Kim <yeajin7933@gmail.com>
2023-08-13 01:22:01 +0900

    Update Header 삭제, TeacherLive

--- frontend/src/pages/Teacher/TeacherLiveWriteHint.jsx
+++ frontend/src/pages/Teacher/TeacherLiveWriteHint.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import TeacherHeader from "./TeacherHeader";
 
 const TeacherLiveWriteHint = (props) => {
   const word = props.word;
 
   return (
     <div>
-      <TeacherHeader />
       <main>
         <h1>받아쓰기(단어 보여줌) 페이지</h1>
         <div>{word}</div>
