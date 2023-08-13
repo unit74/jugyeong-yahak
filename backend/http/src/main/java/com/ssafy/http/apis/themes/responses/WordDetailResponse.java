de51900430e696e1fe9efd658f43299ec91aed9d
seongho <tjdghgk98@gmail.com>
2023-08-13 17:10:06 +0900

    fix : .themes/{cyrriclulumId} -> wodExplation 추가됨 - 이미지 없는 테마 요청 -> 이미지 준비중이라는 정보 넣었음
    Former-commit-id: e9554e35628e5ab245bcaee9ac68393672792115

--- backend/http/src/main/java/com/ssafy/http/apis/themes/responses/WordDetailResponse.java
+++ backend/http/src/main/java/com/ssafy/http/apis/themes/responses/WordDetailResponse.java
@@ -2,11 +2,10 @@ package com.ssafy.http.apis.themes.responses;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.ssafy.http.apis.themes.entities.WordEntity;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -14,18 +13,22 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class WordDetailResponse {
 
-    private long id;
-    private String word;
-    private String wordImageUrl;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+  private long id;
+  private String word;
+  //단어 설명
+  private String wordExplanation;
+  private String wordImageUrl;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 
-    public void of(WordEntity wordEntity) {
-        this.id = wordEntity.getId();
-        this.word = wordEntity.getWord();
-        this.wordImageUrl = wordEntity.getWordImageUrl();
-        this.createdAt = wordEntity.getCreatedAt();
-        this.updatedAt = wordEntity.getUpdatedAt();
-    }
+
+  public void of(WordEntity wordEntity) {
+    this.id = wordEntity.getId();
+    this.word = wordEntity.getWord();
+    this.wordExplanation = wordEntity.getWordExplanation();
+    this.wordImageUrl = wordEntity.getWordImageUrl();
+    this.createdAt = wordEntity.getCreatedAt();
+    this.updatedAt = wordEntity.getUpdatedAt();
+  }
 
 }
