0a9fe7943a3b9f5b5fef14186cc18a9aea512116
HB_Kim <ghkdwp129@gmail.com>
2023-08-16 00:06:50 +0900

    드디어 Finally 마참내 TTS

--- frontend/src/pages/Common/CLOVA copy.js
+++ frontend/src/pages/Common/CLOVA copy.js
@@ -1,27 +1,20 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Howl, Howler } from 'howler';
 
 export default function CLOVA({ message }) {
-  // 이놈이 제일 중요했네......
-  // navigator.mediaDevices.getUserMedia 요청을 처리하는 useEffect 
+  const [audioURL, setAudioURL] = useState(null);
+  // Howler.autoUnlock = false;
+
   useEffect(() => {
     console.log('useEffect triggered with message:', message);    
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(stream => {
-        var audioContext = new AudioContext();  // 혹시 몰라서 있는게 좋음
-        fetchTTSMessage(message);
-        // 여기에 원하는 로직을 추가할 수 있습니다.
-        // 예를 들면, 스트림에 관한 작업을 수행하거나 상태를 설정하는 등의 작업을 수행할 수 있습니다.
-      })
-      .catch(err => {
-        console.error('Error accessing audio:', err);
-      });
+    fetchTTSMessage(message);
   }, [message]);
 
-  const playAudio = (url) => {
-    if (url) {
+
+  const playAudio = () => {
+    if (audioURL) {
       const sound = new Howl({
-        src: [url],
+        src: [audioURL],
         format: ['mp3'],
         onplayerror: function() {
           sound.once('unlock', function() {
@@ -56,11 +49,11 @@ export default function CLOVA({ message }) {
       const newAudioURL = URL.createObjectURL(blob);
       console.log('Created audio URL:', newAudioURL);
       
-      playAudio(newAudioURL); // 실행!!!!!!!!!!!!!!!!!!!!!!
+      setAudioURL(newAudioURL);
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
     }
   };
 
-  return null;
+  return null
 }
