02919e22c3d8517256bc8874e032a3f098e148b0
unit74 <u__nit@naver.com>
2023-08-16 12:37:45 +0900

    feat: 추리 퀴즈 페이지 추가

---
+++ frontend/src/pages/Live/LiveStudentGuessQuiz.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useOutletContext } from "react-router-dom";
+
+const LiveStudentGuessQuiz = () => {
+  const timer = useOutletContext().timer;
+
+  return (
+    <div>
+      <h1>✔ 추리 게임</h1>
+      {timer > 0 && <div>{timer}</div>}
+      <div>선생님의 설명을 듣고 단어를 받아쓰기를 통해 알아맞혀 봅시다.</div>
+      <div>오늘 배운 단어 중에 문제가 나옵니다.</div>
+    </div>
+  );
+};
+
+export default LiveStudentGuessQuiz;
