e0f57021af853463437f8eac1c79dd8db88f835d
unit74 <u__nit@naver.com>
2023-08-17 00:34:19 +0900

    refactor: 필요없는 구문 삭제
    Former-commit-id: 84f67ebceb82098c959647af0af2526a0ad08602

--- backend/http/src/main/java/com/ssafy/http/apis/lecture/services/LectureService.java
+++ backend/http/src/main/java/com/ssafy/http/apis/lecture/services/LectureService.java
@@ -1,8 +1,6 @@
 package com.ssafy.http.apis.lecture.services;
 
-import com.ssafy.http.apis.lecture.requests.ControlMicRequest;
 import com.ssafy.http.apis.lecture.requests.ConvertPageRequest;
-import com.ssafy.http.apis.lecture.requests.MoveMouseRequest;
 import java.net.URI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -15,32 +13,6 @@ public class LectureService {
 
     @Value("${sse.server.url}")
     private String BASE_URL;
-
-    public void controlMic(ControlMicRequest controlMicRequest) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpEntity<ControlMicRequest> request = new HttpEntity<>(controlMicRequest);
-
-        URI uri = UriComponentsBuilder.fromHttpUrl(BASE_URL)
-                                      .path("/mic/control")
-                                      .build()
-                                      .toUri();
-
-        restTemplate.postForLocation(uri, request);
-    }
-
-    public void moveMousePointer(MoveMouseRequest moveMouseRequest) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpEntity<MoveMouseRequest> request = new HttpEntity<>(moveMouseRequest);
-
-        URI uri = UriComponentsBuilder.fromHttpUrl(BASE_URL)
-                                      .path("/mouse/pointer")
-                                      .build()
-                                      .toUri();
-
-        restTemplate.postForLocation(uri, request);
-    }
 
     public void convertPage(ConvertPageRequest convertPageRequest) {
         RestTemplate restTemplate = new RestTemplate();
