441ec3d033b3b435632173a95aec0b6c641ea803
seongho <tjdghgk98@gmail.com>
2023-08-16 14:30:00 +0900

    fix : requestparam -> requestpart로 수정했을때 되는지
    Former-commit-id: a148c483b3775de68e4ef76411a28ebb98fa9e86

--- backend/http/src/main/java/com/ssafy/http/apis/members/controllers/MemberPrivateController.java
+++ backend/http/src/main/java/com/ssafy/http/apis/members/controllers/MemberPrivateController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -80,7 +79,7 @@ public class MemberPrivateController {
       MediaType.MULTIPART_FORM_DATA_VALUE})
   public ResponseEntity<?> registerStudents(
       @Valid @RequestPart StudentRegisterRequest studentRegisterRequest,
-      @RequestParam("faceImage") MultipartFile faceImage) { //@RequestPart
+      @RequestPart("faceImage") MultipartFile faceImage) { //@RequestParam
     System.out.println("학생 등록 요청 받음");
 
     memberService.registerStudents(SecurityUtil.getLoginUserId(), faceImage,
