cb4e1fc655fd0bc45860b966c6aae8e42d26b7dc
unit74 <u__nit@naver.com>
2023-08-17 15:53:15 +0900

    fix: 안정성 체크 추가
    Former-commit-id: 1388c983fc8cb37ac765bca1dea989709d6e2f3a

--- frontend/src/pages/Live/LiveWriteWord.jsx
+++ frontend/src/pages/Live/LiveWriteWord.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { useOutletContext } from "react-router-dom";
 
 const LiveWriteWord = () => {
-  const word = useOutletContext().word;
-
   return (
     <div>
       <h1>✔ 불러주는 단어를 공책에 적어봅시다.</h1>
