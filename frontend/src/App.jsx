5c5188dfb0a1664d3d3b05b900617cdbeabcc0d0
unit74 <u__nit@naver.com>
2023-08-17 10:46:46 +0900

    feat: 항상 재로그인

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -78,6 +78,20 @@ import LiveStudentChoseongQuiz from "./pages/Live/LiveStudentChoseongQuiz";
 import FaceLogin from "./pages/Common/FaceLogin";
 
 function App() {
+  useEffect(() => {
+    // 이벤트 핸들러 함수 정의
+    const handleBeforeUnload = (event) => {
+      localStorage.removeItem("userInfo");
+      localStorage.removeItem("accessToken");
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
   return (
     <Provider store={store}>
       <Routes>
