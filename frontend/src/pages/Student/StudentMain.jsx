83604c4c1ef59aaaafb5f093fa4819ad730b55d9
nayeon <kny0628m@gmail.com>
2023-08-18 09:40:17 +0900

    fix : diary-main 성별오류
    Former-commit-id: a16b44df3c26cde4d524508f7d07a265f8edf73d

--- frontend/src/pages/Student/StudentMain.jsx
+++ frontend/src/pages/Student/StudentMain.jsx
@@ -5,16 +5,21 @@ import useTimeoutCallback from "../Common/hooks/useTimeoutCallback";
 import TTSsentence from "../Common/TTSsentence";
 import { Transition } from "react-transition-group";
 import styled from "@emotion/styled";
+import { useDispatch } from "react-redux";
 
 import liveImg from "../../assets/images/live.png";
 import reviewImg from "../../assets/images/studentreview.png";
 import diaryImg from "../../assets/images/diary.png";
+import { setGender } from "../../store/actions/setGenderAction";
 
 export default function StudentMain() {
   const navigate = useNavigate();
   const [fade, setFade] = useState(false);
-  const [userInfo, setUserInfo] = useState(JSON.parse(localStorage.getItem("userInfo")));
+  const [userInfo, setUserInfo] = useState(
+    JSON.parse(localStorage.getItem("userInfo"))
+  );
   const [msg, setMsg] = useState(null);
+  const dispatch = useDispatch();
 
   const ttsMaker = async (msg, timer) => {
     return new Promise((resolve) => {
@@ -37,7 +42,7 @@ export default function StudentMain() {
       let text = "";
 
       if (userInfo !== null) {
-        console.log(userInfo.gender);
+        dispatch(setGender(userInfo.gender));
         let gender = userInfo.gender === 0 ? "어머님" : "아버님";
         console.log(userInfo.gender);
         console.log(gender);
@@ -92,7 +97,8 @@ export default function StudentMain() {
   const lectureTime = userInfo ? userInfo.lectureTime : "10:00:00";
   const lectureTimeParts = userInfo && lectureTime.split(":");
   const lectureStartMinutes =
-    userInfo && parseInt(lectureTimeParts[0], 10) * 60 + parseInt(lectureTimeParts[1], 10);
+    userInfo &&
+    parseInt(lectureTimeParts[0], 10) * 60 + parseInt(lectureTimeParts[1], 10);
 
   // 강의 시작 30분 전의 시간
   const lecturePreStartMinutes = lectureStartMinutes - 30;
@@ -102,7 +108,8 @@ export default function StudentMain() {
 
   // 조건 확인
   const showEnterClass =
-    currentMinutes >= lecturePreStartMinutes && currentMinutes < lectureEndMinutes;
+    currentMinutes >= lecturePreStartMinutes &&
+    currentMinutes < lectureEndMinutes;
 
   return (
     <div className={`${styles.main} ${fade ? styles.fadeOut : ""}`}>
@@ -117,7 +124,11 @@ export default function StudentMain() {
         {showEnterClass ? (
           <div className={styles.time}>
             <div className={styles.timeImg}>
-              <img className={styles.responsive_image} src={liveImg} alt="liveImg" />
+              <img
+                className={styles.responsive_image}
+                src={liveImg}
+                alt="liveImg"
+              />
             </div>
             <button className={styles.clearButton} onClick={navigateToLive}>
               교실에 들어가기
@@ -126,16 +137,27 @@ export default function StudentMain() {
         ) : (
           <div className={styles.time}>
             <div className={styles.timeImg}>
-              <img className={styles.responsive_image} src={reviewImg} alt="reviewImg" />
+              <img
+                className={styles.responsive_image}
+                src={reviewImg}
+                alt="reviewImg"
+              />
             </div>
-            <button className={styles.clearButton} onClick={navigateToRecordDictation}>
+            <button
+              className={styles.clearButton}
+              onClick={navigateToRecordDictation}
+            >
               혼자 공부하기
             </button>
           </div>
         )}
         <div className={styles.time}>
           <div className={styles.timeImg}>
-            <img className={styles.responsive_image} src={diaryImg} alt="diaryImg" />
+            <img
+              className={styles.responsive_image}
+              src={diaryImg}
+              alt="diaryImg"
+            />
           </div>
           <button className={styles.diaryButton} onClick={navigateToDiaryList}>
             일기장 보기
